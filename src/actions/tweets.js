c9b16829a3e0ccd69dccf70aca2c5d6779039ff5
Shea Mentges <shea.mentges@gmail.com>
2020-03-21 21:53:24 -0700

    Added routing.

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -24,7 +24,7 @@ export function handleAddTweet(text, replyingTo) {
             replyingTo
         })
             .then((tweet) => dispatch(addTweet(tweet)))
-            .then(() => dispatch(hideLoading))
+            .then(() => dispatch(hideLoading()))
     }
 }
 
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { formatTweet, formatDate } from '../utils/helpers'
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti'
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component {
     handleLike = (e) => {
@@ -18,7 +19,7 @@ class Tweet extends Component {
     }
     toParent = (e, id) => {
         e.preventDefault()
-        // todo: Redirect to parent tweet
+        this.props.history.push(`/tweet/${id}`)
     }
     render() {
         const { tweet } = this.props
@@ -28,10 +29,10 @@ class Tweet extends Component {
         }
 
         const {
-            name, avatar, timestamp, text, hasLiked, likes, replies, parent
+            name, avatar, timestamp, text, hasLiked, likes, id, replies, parent
         } = tweet
         return (
-            <div className='tweet'>
+            <Link to={`/tweet/${id}`} className='tweet'>
                 <img
                     src={avatar}
                     alt={`Avatar of ${name}`}
@@ -61,7 +62,7 @@ class Tweet extends Component {
                         </div>
                     </div>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
@@ -78,4 +79,4 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
     }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
